a3eaa6e6892de29a1edb2d04f6144b4245e8c3c7
Mohammad Pishahang <64814209+mhpishahang@users.noreply.github.com>
2024-01-14 02:57:53 -0800

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,7 +3,9 @@
 echo "Installing system dependencies for SUMO"
 
 # Quick check that we actually have brew, if not, lets install it
-command -v brew >/dev/null 2>&1 || echo >&2 "Homebrew is missing, you can install it by running \n/usr/bin/ruby -e \$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 1
+command -v brew >/dev/null 2>&1 || 
+  echo "Homebrew is missing. Let's install it." || 
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # script dependencies
 # brew install wget
