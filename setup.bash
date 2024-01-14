cb15c0157720fe219e28dd9dd6e8315da533b129
Mohammad Pishahang <64814209+mhpishahang@users.noreply.github.com>
2024-01-14 03:54:54 -0800

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,10 +1,44 @@
 #!/bin/bash
 
-echo "Installing system dependencies for SUMO"
+ring_bell() {
+  # Use the shell's audible bell.
+  if [[ -t 1 ]]
+  then
+    printf "\a"
+  fi
+}
 
-# Quick check that we actually have brew, if not, lets install it
+ohai "Installing system dependencies for SUMO"
+
+# Quick check that we actually have brew
 command -v brew >/dev/null 2>&1 || 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "https://github.com/Homebrew/brew/releases/download/4.2.3/Homebrew-4.2.3.pkg" || 
+  echo "after installing Homebrew run this command again." || 
+  exit 1
+  
+
+if [[ "${OS}" == "Darwin" ]]
+then
+  echo "we are on macos"
+fi
+case "${SHELL}" in
+  */bash*)
+    shell_rcfile="${HOME}/.bash_profile"
+    ;;
+  */zsh*)
+    shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zprofile"
+    ;;
+  */fish*)
+    shell_rcfile="${HOME}/.config/fish/config.fish"
+    ;;
+  *)
+    shell_rcfile="${ENV:-"${HOME}/.profile"}"
+    ;;
+esac
+
+echo ${shell_rcfile}
 
 # script dependencies
 # brew install wget
+
+ring_bell
